deeffa24d0c13b777c36c10b7361a913c532267c
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2023-03-04 20:54:49 -0500

    Update

--- leetcode-contests/test.py
+++ leetcode-contests/test.py
@@ -8,6 +8,6 @@ options.add_argument("--headless=new")
 driver = webdriver.Chrome(chrome_options=options)
 driver.get("https://leetcode.com/contest/weekly-contest-334/")
 soup = BeautifulSoup(driver.page_source, "html.parser")
-for ul in soup.find_all('ul'):
+for ul in soup.find_all('ul', {"class": "list-group hover-panel contest-question-list"}):
     print(ul)
 driver.quit()
