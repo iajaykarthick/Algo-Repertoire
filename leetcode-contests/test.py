39e37d3816719c35e180ec6a2830316a0711df9f
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2023-03-04 21:01:12 -0500

    Update

--- leetcode-contests/test.py
+++ leetcode-contests/test.py
@@ -5,9 +5,9 @@ from lxml import etree
 
 options = Options()
 options.add_argument("--headless=new")
-driver = webdriver.Chrome(chrome_options=options)
+driver = webdriver.Chrome(options=options)
 driver.get("https://leetcode.com/contest/weekly-contest-334/")
 soup = BeautifulSoup(driver.page_source, "html.parser")
-for li in soup.find_all('ul', {"class": "list-group hover-panel contest-question-list"})[0].find_all("li"):
-    print(li)
+for li in soup.find_all('ul', {"class": "list-group hover-panel contest-question-list"})[0].find_all("li", {"class": "list-group-item"}):
+    print(li.find('a')['href'])
 driver.quit()
