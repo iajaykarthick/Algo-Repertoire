4dde61d96b7f500db5ecf67c47f6f5f1f3f6ce6d
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2023-03-04 20:47:39 -0500

    Update

--- leetcode-contests/test.py
+++ leetcode-contests/test.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from lxml import etree
+
 options = Options()
 options.headless = True
 driver = webdriver.Chrome(chrome_options=options)
 driver.get("https://leetcode.com/contest/weekly-contest-334/")
 soup = BeautifulSoup(driver.page_source, "html.parser")
-print(soup)
+dom = etree.HTML(str(soup))
+print(dom.xpath('"//*[@id="contest-app"]/div/div/div[4]/div[1]/ul/li[1]')[0].text)
