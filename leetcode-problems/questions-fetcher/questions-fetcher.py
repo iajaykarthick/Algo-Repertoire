3d358c116e981c20a0bb22f252361c7b5f0c76a0
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2023-03-05 11:30:02 -0500

    Update

--- leetcode-problems/questions-fetcher/questions-fetcher.py
+++ leetcode-problems/questions-fetcher/questions-fetcher.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import requests
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -8,31 +9,46 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-output_files_path = 'leetcode-problems/problems'
+
 
 options = Options()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
-driver.get("https://leetcode.com/problems/longest-palindromic-substring/")
+output_files_path = 'leetcode-problems/problems'
 question_text_xpath = '//*[@id="qd-content"]/div[1]/div/div/div/div[2]/div/div/div[3]/div'
-timeout = 3
-try:
-    element_present = EC.presence_of_element_located((By.XPATH, question_text_xpath))
-    WebDriverWait(driver, timeout).until(element_present)
-    element = driver.find_element(By.XPATH, question_text_xpath)
-    question_text = element.get_attribute('innerHTML')
+
+isExist = os.path.exists(output_files_path)        
+if not isExist:
+    # Create a new directory because it does not exist
+    os.makedirs(output_files_path)
+    print("The new directory is created!")
+
+
+URL = "https://leetcode.com/api/problems/algorithms/"
+response = requests.get(URL)
+data = response.json()
+for question_stat in data['stat_status_pairs'][:10]:
+    question = question_stat['stat']
+    question_id = question['question_id']
+    question_title = question['question__title']
+    question_title_slug = question['question__title_slug']
+    URL = f"https://leetcode.com/problems/{question_title_slug}"
+    filename = f"{output_files_path}/{question_id}_{question_title.replace(' ', '_')}.md"
+    driver.get(URL)
     
-    isExist = os.path.exists(output_files_path)
-    if not isExist:
-        # Create a new directory because it does not exist
-        os.makedirs(output_files_path)
-        print("The new directory is created!")
-    # Writing to file
-    with open(f"{output_files_path}/test.md", "w") as file1:
-        # Writing data to a file
-        file1.write(question_text)
-except TimeoutException:
-    print("Timed out waiting for page to load")
+    timeout = 3
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, question_text_xpath))
+        WebDriverWait(driver, timeout).until(element_present)
+        element = driver.find_element(By.XPATH, question_text_xpath)
+        question_text = element.get_attribute('innerHTML')
+        
+        # Writing to file
+        with open(filename, "w") as file1:
+            # Writing data to a file
+            file1.write(question_text)
+    except TimeoutException:
+        print("Timed out waiting for page to load")
 
 # Writing to file
 with open("leetcode-problems/questions-fetcher/logtrace.log", "w") as file1:
