6fbbb19fb23298ee8a59de11d4a1f7b5c0fbbc86
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2023-03-05 09:49:30 -0500

    update

--- leetcode-contests/test.py
+++ leetcode-contests/test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
 options = Options()
@@ -12,4 +13,6 @@ soup = BeautifulSoup(driver.page_source, "html.parser")
 #     print(div.get_attribute('innerHTML'))
 
 print(soup.title)
+element = driver.find_element(By.XPATH, '//*[@id="qd-content"]/div[1]/div/div/div/div[2]/div/div/div[3]/div')
+print(element.get_attribute('innerHTML'))
 driver.quit()
