2223724272d09e0d37854c8168f1766fff855495
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2023-03-05 09:46:45 -0500

    update

--- leetcode-contests/test.py
+++ leetcode-contests/test.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from lxml import etree
 
 options = Options()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
 driver.get("https://leetcode.com/problems/longest-palindromic-substring/")
 soup = BeautifulSoup(driver.page_source, "html.parser")
-print(soup)
-for div in soup.find_all('div', {"class": "_1l1MA"}):
-    print('test')
-    print(div.get_attribute('innerHTML'))
-print(soup.find_all('div', {"class": "_1l1MA"}))
+
+# for div in soup.find_all('div', {"class": "list-group hover-panel contest-question-list"}):
+#     print(div.get_attribute('innerHTML'))
+        
+
+print(soup.title)
 driver.quit()
