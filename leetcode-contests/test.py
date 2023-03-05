4db3f22de334ba43859b3d35ef199f36a2ffa30d
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2023-03-04 20:35:30 -0500

    Update

--- leetcode-contests/test.py
+++ leetcode-contests/test.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 options = Options()
 options.headless = True
 driver = webdriver.Chrome(chrome_options=options)
-driver.get("https://www.python.org")
-print(driver.title)
+driver.get("https://leetcode.com/contest/weekly-contest-334/")
+soup = BeautifulSoup(driver.page_source, "html.parser")
+print(soup)
